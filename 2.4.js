1a16cbcf22ab953f733566bd3136fea6a4102e02
Muhammed <muhammedkocak.dev@gmail.com>
2019-02-26 19:30:42 +0100

    2.4

---
+++ 2.4.js
@@ -0,0 +1,22 @@
+
+
+
+
+
+
+
+for (let i = 1; i <= 100; i++) {
+	//als 'rest' van i gedeeld door 2 = 0
+	if (i % 2 == 0) {
+		//druk de waarde van i gedeeld door 2 af
+		console.log(i / 2);
+	}
+		
+	//als 'rest' van i gedeeld door 2 = 1
+	if (i % 2 == 1) {
+		//druk de waarde van i vermenigvuldigd met 3 af
+		console.log(i * 3);
+	}
+}
+				
+			
