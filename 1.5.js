5df244a4d0073841044b95b4989a9dcd1cad65fe
Muhammed <muhammedkocak.dev@gmail.com>
2019-02-28 15:50:24 +0100

    3.1

--- 1.5.js
+++ 1.5.js
@@ -4,6 +4,6 @@ const readlineSync = require("readline-sync");
 let math = parseFloat(readlineSync.question('How much does gas cost in the Netherlands ?'));
 let math1 = parseFloat(readlineSync.question('And how much does gasoline cost in the Netherlands?'));
 
-let total = parseInt(math)*math1;
+//let total = parseFloat(math)*math1;
 
-console.log("The result is "+total);
+console.log("The result is "+math*math1);
